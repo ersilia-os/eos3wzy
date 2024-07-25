b91f21c2be4840ab99307982c2679208f8673e14
LauraGomezjurado <102627399+LauraGomezjurado@users.noreply.github.com>
2024-07-24 18:28:43 -0700

    Update main.py

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,8 +2,7 @@
 import os
 import csv
 import sys
-from cli import parse_arguments, extract_pka_statistics_from_sdf
-
+from cli import main_smiles
 
 # parse arguments
 input_file = sys.argv[1]
@@ -44,29 +43,12 @@ tmp_input_file = os.path.join(root, "tmp_input.csv")
 convert_input_file(input_file, tmp_input_file)
 
 
-# RUN QUPKAKE
 # Create a list of arguments as if they were passed from the command line
 qupkake_args = ["file", tmp_input_file, "-o", "qupkake_output.sdf"]
 
-# Parse the arguments
-parsed_args = parse_arguments(qupkake_args)
 
-# Construct the full paths for the SDF and CSV files
-base_dir = os.getcwd()
-sdf_file = os.path.join(base_dir, 'data/output/qupkake_output.sdf')
-csv_file = os.path.join(base_dir, 'data/output/predicted_pKa_values.csv')
-
-# Check if the SDF file was created successfully (if protonation/deprotonation site was found)
-if not os.path.exists(sdf_file):
-    extract_pka_statistics_from_sdf(sdf_file, csv_file, no_protonation=True)
-    sys.exit(1)
-
-# Convert the SDF file to CSV
-extract_pka_statistics_from_sdf(sdf_file, csv_file, no_protonation=False)
-
-# Remove the intermediate SDF file
-if os.path.exists(sdf_file):
-    os.remove(sdf_file)
+# RUN QUPKAKE
+main_smiles(qupkake_args)
 
 # CLEAN UP TEMPORARY FILE
 os.remove(tmp_input_file)
