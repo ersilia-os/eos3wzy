bbba70a49f4a9cc2e9f0b4eef219fa765203b7d9
Laura Gomezjurado G <lauragomez@Lauras-MacBook-Pro-2.local>
2024-08-19 10:17:52 -0400

    Update run.sh

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,6 +1,6 @@
 cwd=$(pwd)
 cd $1
 qupkake file $2 -s smiles -o intermediate_output.sdf
-python code/postprocess.py data/outputs/intermediate_output.sdf $3
+python code/postprocess.py data/output/intermediate_output.sdf $3
 rm -rf data
 cd $cwd
