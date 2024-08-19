65714122ba86451e8cfe849791f0886685395e41
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-08-20 00:17:18 +0200

    Update run.sh

--- model/framework/run.sh
+++ model/framework/run.sh
@@ -1,6 +1,8 @@
 cwd=$(pwd)
+input_path=$(realpath "$2")
+output_path=$(realpath "$3")
 cd $1
-qupkake file $2 -s smiles -o intermediate_output.sdf
-python code/postprocess.py data/output/intermediate_output.sdf $3
+qupkake file $input_path -s smiles -o intermediate_output.sdf
+python code/postprocess.py data/output/intermediate_output.sdf $output_path
 rm -rf data
 cd $cwd
